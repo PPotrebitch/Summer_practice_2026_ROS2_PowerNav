1444be4faf1ed5a5ed00b34fbce2acd47a223dd0
PPotrebitch <potrebichpavel02@mail.ru>
2026-06-29 20:09:44 +0300

    Update setup.py v.2.0

--- ws_ros2/src/power_nav_robot/setup.py
+++ ws_ros2/src/power_nav_robot/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch..py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
@@ -29,7 +29,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # Сюда позже добавим наши python скрипты (ноды)
+            'battery_monitor = power_nav_robot.battery_monitor:main', 
         ],
     },
 )
