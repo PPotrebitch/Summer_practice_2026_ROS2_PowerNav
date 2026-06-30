56f7cd404d1f380f0498da08d3efb26d13ee60ce
PPotrebitch <potrebichpavel02@mail.ru>
2026-06-30 04:23:50 +0300

    add fimal.launch.py

---
+++ ws_ros2/src/power_nav_robot/launch/final.launch.py
@@ -0,0 +1,78 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    package_name = 'power_nav_robot'
+    pkg_path = get_package_share_directory(package_name)
+
+    world_f = os.path.join(pkg_path, 'worlds', 'warehouse.world')
+    xacro_f = os.path.join(pkg_path, 'urdf', 'robot_model.xacro')
+    bridge_config = os.path.join(pkg_path, 'config', 'ros_gz_bridge.yaml')
+    rviz_config = os.path.join(pkg_path, 'rviz', 'final_rviz.rviz')
+
+    robot_description = Command(['xacro ', xacro_f])
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments={
+            'gz_args': ['-r -v 4 ', world_f],
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': ParameterValue(robot_description, value_type=str),
+            'use_sim_time': True
+        }]
+    )
+    
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'power_nav_robot',
+            '-topic', '/robot_description',
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.1'
+        ],
+        output='screen'
+    )
+
+    gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args', '-p',
+            f'config_file:={bridge_config}'
+        ],
+        parameters=[{'use_sim_time': True}],
+        output='screen'
+    )
+    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': True}],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gz_sim,
+        robot_state_publisher,
+        spawn_robot,
+        gz_bridge,
+        rviz_node
+    ])
